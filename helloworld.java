9d3c8ff8fd624983b0dc7bf711caf7f6ed5e14b3
vallala1503 <vallalasoumya1503@gmail.com>
2025-07-09 02:25:58 +0530

    modified

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class hell0world {
+public class helloworld {
   public static void main(String[] args) {
     System.out.println("Hello,devops World");
   }
